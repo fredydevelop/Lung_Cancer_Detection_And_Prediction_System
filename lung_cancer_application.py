b1bb1c9dded9df3e33b07360aad3aa8b2e06f6f2
Coded Coder <105555015+fredydevelop@users.noreply.github.com>
2024-02-13 11:08:28 +0100

    Update lung_cancer_application.py

--- lung_cancer_application.py
+++ lung_cancer_application.py
@@ -56,9 +56,9 @@ def LungDetector(givendata):
     std_X_resample=std_scaler_loaded.transform(input_data_reshaped)
     prediction = loaded_model.predict(std_X_resample)
     if prediction==1 or prediction=="1":
-      return "signs of lung cancer detected \n kindly go for medical support"
+      return "signs of lung cancer detected, kindly visit the hospital for medical support"
     else:
-      return "No sign of Lung Cancer Detected \n Maintain a healthy lifestyle"
+      return "No sign of Lung Cancer Detected"
     
  
 #main function handling the input
